e5fb30ff22abfb64df04a65778fc068c4612e9d6
Anna Mahe <annamahe9@gmail.com>
2023-03-01 14:22:05 +0100

    started new pet form

---
+++ app/controllers/owner/pets_controller.rb
@@ -0,0 +1,20 @@
+class Owner::PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(list_params)
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:pet).permit(:name, :species, :description, :price_per_day, :age, :owner, :photo)
+  end
+end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,7 +4,7 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
-  
+
   def show
     @pet = Pet.find(params[:id])
     @booking = Booking.new
