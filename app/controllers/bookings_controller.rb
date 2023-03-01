0b8b0f9575349eae2add92173734008a3776a7bc
Eva Touboulic <etouboulic@gmail.com>
2023-03-01 14:21:18 +0100

    Fixed footer + Add bookings page for renter + style bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = current_user.bookings.all
+  end
+
   def create
     @pet = Pet.find(params[:pet_id])
     @booking = Booking.new(params_booking)
@@ -6,6 +10,7 @@ class BookingsController < ApplicationController
     @booking.status = "En attente"
       @booking.renter = current_user
     @booking.total_price = @pet.price_per_day * (@booking.ends_on - @booking.starts_on)
+
     if @booking.save
       flash[:notice] = "Tout s'est bien passé, la réservation est enregistrée"
       redirect_to pets_path
